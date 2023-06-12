880b968194815db05de7801dc707262b146de8f2
shubendu <biswasshubendu4@gmail.com>
2023-06-12 22:48:07 +0530

    changes in user resources file

--- resources/user.py
+++ resources/user.py
@@ -38,8 +38,10 @@ class UserRegister(MethodView):
     @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
         if UserModel.query.filter(
-            or_(UserModel.username == user_data["username"], 
-                UserModel.email == user_data["email"])).first():
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"]
+            )).first():
             abort(409, message="A user with that username or email already exists.")
 
         user = UserModel(
